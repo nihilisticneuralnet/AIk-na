28f41175b836f45cac388e8714068741ad924756
parth <138315505+nihilisticneuralnet@users.noreply.github.com>
2025-02-28 13:40:59 +0530

    Create query.py

---
+++ src/query.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+def query(text, model_id="tiiuae/falcon-7b-instruct"):
+    api_url = f"https://api-inference.huggingface.co/models/{model_id}"
+    headers = {"Authorization": f"Bearer {os.environ['HF_TOKEN']}"}
+    payload = {"inputs": text}
+
+    print(f"Querying...: {text}")
+    response = requests.post(api_url, headers=headers, json=payload)
+    
+    if response.status_code == 200:
+        return response.json()[0]["generated_text"][len(text) + 1 :]
+    else:
+        return "Error in AI response."
